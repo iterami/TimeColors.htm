8a219ca2bbd0e254082b05d17e3908f5dc7af045
honzi <czechinthemail@gmail.com>
2017-05-29 15:28:10 -0700

    added core_init(), repo_init(), and began global inputs

--- js/timecolors.js
+++ js/timecolors.js
@@ -1,22 +1,19 @@
 'use strict';
 
 function update_color(){
-    // Current time in time units set by the update interval.
     var current_time = Math.floor(time_date_to_timestamp() / interval).toString();
-
     var hex = '#' + current_time.substr(current_time.length - 6);
 
     document.body.style.background = hex;
     document.getElementById('current-color').innerHTML = hex;
 }
 
-// Update interval in milliseconds.
-var interval = 100;
-
-window.onload = function(e){
+function repo_init(){
     update_color();
     window.setInterval(
       update_color,
       interval
     );
-};
+}
+
+var interval = 100;
