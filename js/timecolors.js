780cec1f94b1a0ae3527a5f03dc4d5bd6ea38546
honzi <czechinthemail@gmail.com>
2017-06-08 14:08:24 -0700

    added core_repo_init() usage

--- js/timecolors.js
+++ js/timecolors.js
@@ -9,6 +9,10 @@ function update_color(){
 }
 
 function repo_init(){
+    core_repo_init({
+      'title': 'TimeColors.htm',
+    });
+
     update_color();
     window.setInterval(
       update_color,
