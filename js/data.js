c1b24c01f1b4053135c925d4fbaf406df095e011
honzi <czechinthemail@gmail.com>
2017-10-15 18:44:07 -0700

    added pause

--- js/data.js
+++ js/data.js
@@ -1,6 +1,10 @@
 'use strict';
 
 function update_color(){
+    if(core_menu_open){
+        return;
+    }
+
     var current_time = Math.floor(time_date_to_timestamp() / interval).toString();
     var hex = '#' + current_time.substr(current_time.length - 6);
 
