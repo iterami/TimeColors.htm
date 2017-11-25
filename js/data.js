0ba3760184628ea24833c41d5aed7a2aefce261d
Jan Hořava <czechinthemail@gmail.com>
2017-11-24 16:50:50 -0800

    removed time.js

--- js/data.js
+++ js/data.js
@@ -5,7 +5,7 @@ function update_color(){
         return;
     }
 
-    var current_time = Math.floor(time_date_to_timestamp() / core_storage_data['interval']).toString();
+    var current_time = Math.floor(core_date_to_timestamp() / core_storage_data['interval']).toString();
     var hex = '#' + current_time.substr(current_time.length - 6);
 
     document.body.style.background = hex;
