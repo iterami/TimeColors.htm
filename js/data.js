79019facaede01184376441e4f70e97f600ca1b2
Jan Hořava <czechinthemail@gmail.com>
2018-07-07 17:06:41 -0700

    changed var to let

--- js/data.js
+++ js/data.js
@@ -5,8 +5,8 @@ function update_color(){
         return;
     }
 
-    var current_time = Math.floor(core_date_to_timestamp() / core_storage_data['interval']).toString();
-    var hex = '#' + current_time.substr(current_time.length - 6);
+    let current_time = Math.floor(core_date_to_timestamp() / core_storage_data['interval']).toString();
+    let hex = '#' + current_time.substr(current_time.length - 6);
 
     document.body.style.background = hex;
     document.getElementById('current-color').innerHTML = hex;
