625677b35082e08a19f61530023f279845539eb0
Jan Hořava <czechinthemail@gmail.com>
2019-07-15 21:37:33 -0700

    updated time usage

--- js/data.js
+++ js/data.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function update_color(){
-    let current_time = Math.floor(core_date_to_timestamp() / core_storage_data['interval']).toString(16);
+    let current_time = Math.floor(date_to_timestamp() / core_storage_data['interval']).toString(16);
     let hex = '#' + current_time.substr(current_time.length - 6);
 
     document.body.style.background = hex;
