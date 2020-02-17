3462724e3178f9ee88007a7e32033ffdde7b25b4
Jan Hořava <czechinthemail@gmail.com>
2020-02-16 23:08:08 -0800

    replaced some let with const

--- js/data.js
+++ js/data.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function update_color(){
-    let current_time = Math.floor(date_to_timestamp() / core_storage_data['interval']).toString(16);
-    let hex = '#' + current_time.substr(current_time.length - 6);
+    const current_time = Math.floor(date_to_timestamp() / core_storage_data['interval']).toString(16);
+    const hex = '#' + current_time.substr(current_time.length - 6);
 
     document.body.style.background = hex;
 
