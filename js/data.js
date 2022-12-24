c266403137352d78f5ad82398db80466e7e87c39
Jan Hořava <czechinthemail@gmail.com>
2022-12-23 19:32:26 -0800

    changed background to background color

--- js/data.js
+++ js/data.js
@@ -4,7 +4,7 @@ function update_color(){
     const current_time = Math.floor(date_to_timestamp() / core_storage_data['interval']).toString(16);
     const hex = '#' + current_time.substr(current_time.length - 6);
 
-    document.body.style.background = hex;
+    document.body.style.backgroundColor = hex;
 
     core_ui_update({
       'ids': {
