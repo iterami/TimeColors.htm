7b6ffa1441a881c2fe3d3c3107380aea1c99cd43
Jan Hořava <czechinthemail@gmail.com>
2023-07-18 15:13:20 -0700

    combined js into repo.js

--- repo.js
+++ repo.js
@@ -19,3 +19,17 @@ function repo_init(){
       'todo': update_color,
     });
 }
+
+function update_color(){
+    const current_time = Math.floor(date_to_timestamp() / core_storage_data['interval']).toString(16);
+    const hex = '#' + current_time.substr(current_time.length - 6);
+
+    document.body.style.backgroundColor = hex;
+
+    core_ui_update({
+      'ids': {
+        'color': hex,
+        'color-info': hex,
+      },
+    });
+}
