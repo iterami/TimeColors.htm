7d88811fefda108a9e26378c051ad677b80bd73f
Jan Hořava <czechinthemail@gmail.com>
2023-10-23 21:58:43 -0700

    replaced substr() with substring()

--- repo.js
+++ repo.js
@@ -22,7 +22,7 @@ function repo_init(){
 
 function update_color(){
     const current_time = Math.floor(date_to_timestamp() / core_storage_data['interval']).toString(16);
-    const hex = '#' + current_time.substr(current_time.length - 6);
+    const hex = '#' + current_time.substring(current_time.length - 6);
 
     document.body.style.backgroundColor = hex;
 
