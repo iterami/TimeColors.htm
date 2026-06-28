ebd9e763c6a79fb595a6c6d3c4ea5759360afe19
Jan Hořava <czechinthemail@gmail.com>
2026-06-28 13:31:14 -0700

    displayed time and color in ui and title

--- repo.js
+++ repo.js
@@ -2,17 +2,16 @@
 
 function repo_init(){
     core_repo_init({
-      'info': '<span id=color-info></span>',
+      'info': '<span class=timecolor></span>',
       'storage': {
         'interval': 100,
       },
       'storage_menu': '<table><tr><td><input class=mini id=interval min=1 step=any type=number><td>Interval</table>',
       'title': 'TimeColors.htm',
-      'ui': '<span id=color></span>',
+      'ui': '<span id=timecolor></span>',
     });
 
     update_color();
-
     core_interval_modify({
       'id': 'update',
       'interval': core_storage_data.interval,
@@ -21,15 +20,19 @@ function repo_init(){
 }
 
 function update_color(){
-    const current_time = Math.floor(date_to_timestamp() / core_storage_data.interval).toString(16);
+    const timestamp = date_to_timestamp();
+    const current_time = Math.floor(timestamp / core_storage_data.interval).toString(16);
     const hex = '#' + current_time.substring(current_time.length - 6);
+    const timecolor = time_format({
+      'date': timestamp_to_date(timestamp),
+    }) + ' ' + hex;
 
     document.body.style.backgroundColor = hex;
-
+    document.title = timecolor;
     core_ui_update({
+      'classname': true,
       'ids': {
-        'color': hex,
-        'color-info': hex,
+        'timecolor': timecolor,
       },
     });
 }
